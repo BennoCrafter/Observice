f9288149b37fee1b0004ce0fd7cf8c86a991596a
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-08-25 13:51:35 +0200

    Update get_current_timestamp.py

--- src/utils/get_current_timestamp.py
+++ src/utils/get_current_timestamp.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 def get_current_timestamp():
     now = datetime.now()
-    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = now.strftime("%Y-%m-%d-%H-%M-%S")
     return timestamp
