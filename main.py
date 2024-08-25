55be84712aa5537dfea2fc715a6fe71b68bb5bdf
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-08-25 18:05:49 +0200

    Update run.sh && Update auto restating at 4 am
    Added internet connection check and auto restarting on failure.

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from threading import Thread
 import time
 from src.threads_manager import ThreadsManager
+import schedule
+import subprocess
+
+
+def restart():
+    subprocess.call(["sudo", "reboot"])
+
+def auto_restarting():
+    schedule.every().day.at("04:00").do(restart)
+
+    print("Scheduler started. The computer will restart at 4 AM daily.")
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)  # Check every minute
 
 if __name__ == '__main__':
     threads_manager = ThreadsManager()
+    threads_manager.add_new_thread(target=auto_restarting, name="Auto restarting at 4 am.")
     threads_manager.start_threads()
--- run.sh
+++ run.sh
@@ -6,27 +6,51 @@ cleanup() {
     kill $(jobs -p)
 }
 
+# Function to check for an active internet connection
+check_internet() {
+    echo "Checking for an internet connection..."
+    while ! ping -q -c 1 -W 1 8.8.8.8 >/dev/null; do
+        echo "No internet connection detected. Waiting..."
+        sleep 10  # Wait and recheck after 10 seconds
+    done
+    echo "Internet connection detected. Continuing execution."
+}
+
+# Function to start a script and auto-restart on failure
+start_script() {
+    local script_name=$1
+    local python_version=$2
+
+    while true; do
+        check_internet  # Ensure there is an internet connection before starting the script
+        echo "Starting $script_name..."
+        $python_version $script_name
+
+        if [ $? -ne 0 ]; then
+            echo "$script_name failed. Waiting and Restarting..."
+            sleep 60
+        fi
+
+        # After the script fails, check again if there is an internet connection
+        check_internet
+    done
+}
+
 # Trap script exit to run cleanup
 trap cleanup EXIT
 
-# Check if an argument is provided
+# Check if an argument is provided for the Python version
 if [ -z "$1" ]; then
-    # If no argument, use default
-    echo "No argument provided. Using default python version."
+    echo "No argument provided. Using default Python version."
     python_version="python"
 else
     echo "Using: $1"
     python_version="$1"
 fi
 
+# Start scripts and auto-restart on failure
+start_script "main.py" $python_version &
+start_script "discord_bot.py" $python_version &
 
-echo "Starting Observice!"
-$python_version main.py &  # Run main.py in the background
-main_pid=$!
-
-$python_version discord_bot.py &  # Run discord_bot.py in the background
-bot_pid=$!
-
-# Wait for the processes to finish
-wait $main_pid
-wait $bot_pid
+# Wait for all background processes to finish
+wait
